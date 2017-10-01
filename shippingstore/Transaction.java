9ac253b366560efe32e1f5ff555bc0994b747763
Emily Beaudoin <erb64@txstate.edu>
2017-10-01 16:07:29 -0500

    started transaction database

---
+++ shippingstore/Transaction.java
@@ -0,0 +1,12 @@
+package shippingstore;
+
+public class Transaction 
+{
+	private final Integer customerID;
+	private final String trackingNumber;
+	private final Date shippingDate;
+	private final Date deliverDate;
+	private final Float cost;
+	private final Integer employeeID;
+
+}
